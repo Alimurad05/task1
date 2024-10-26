58286d64b87af290703abed9a3aad8bed2bbcc8e
User <alimuradof93@gmail.com>
2024-10-26 17:53:52 +0400

    task1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+            int number1=15;
+            int number2=7;
+            int cem=number1+number2;
+        int hasil=number1*number2;
+        int bolme=number1/number2;
+        int qaliq= number1%number2;
+        int cixma=number1-number2;
+        System.out.println(cem);
+        System.out.println(hasil);
+        System.out.println(bolme);
+        System.out.println(qaliq);
+        System.out.println(cixma);
+        }
+    }
